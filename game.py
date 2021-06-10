02419d29e5b5a568c94a48989286a45bf6715349
Esteban Carter <carterea1999@gmail.com>
2021-06-10 13:37:10 -0400

    Created Pirateer Class

--- game.py
+++ game.py
@@ -1,16 +1,28 @@
+class Pirate:
+    def __init__(self):
+        self.name = input('Hurry up and enter your scallywags name: ')
+        self.first_name = 'Bootleg'
+
+    def show_full_name(self):
+        print(self.first_name + ' ' + self.name.capitalize())
+
 def play():
 
-    def room_name():
-        print('narrative')
-        answer = input(>)
-if answer == '1':
-    callfunction()
-if answer == '2':
-    callfunction()
-if answer == '3':
-    callfunction()
-if answer == '4':
-    callfunction()
-
-
+    player = Pirate()
+    player.show_full_name()
+#
+#
+# #     def room_name():
+# #         print('narrative')
+# #         answer = input(>)
+# # if answer == '1':
+# #     callfunction()
+# # if answer == '2':
+# #     callfunction()
+# # if answer == '3':
+# #     callfunction()
+# # if answer == '4':
+# #     callfunction()
+# #
+# #
 play()
