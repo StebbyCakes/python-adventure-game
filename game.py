9c42c6a8b72983fb10f7a43bcd3dbbc46227cbf8
Esteban Carter <carterea1999@gmail.com>
2021-06-10 11:29:07 -0400

    creating a room template

---
+++ game.py
@@ -0,0 +1,16 @@
+def play():
+
+    def room_name():
+        print('narrative')
+        answer = input(>)
+if answer == '1':
+    callfunction()
+if answer == '2':
+    callfunction()
+if answer == '3':
+    callfunction()
+if answer == '4':
+    callfunction()
+
+
+play()
